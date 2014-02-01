56a9957cc59b9a179afe11ea7c6879ad2becfc67
Ivan Sugerman <ivan@ivansugerman.com>
2014-02-01 16:52:53 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt){
+
+	"use strict";
+
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        htmlhint: {
+		    build: {
+		        options: {
+		            'tag-pair': true,
+		            'tagname-lowercase': true,
+		            'attr-lowercase': true,
+		            'attr-value-double-quotes': true,
+		            'doctype-first': true,
+		            'spec-char-escape': true,
+		            'id-unique': true,
+		            'head-script-disabled': true,
+		            'style-disabled': true
+		        },
+		        src: ['index.html']
+		    }
+		},
+
+		cssc: {
+		    build: {
+		        options: {
+		            consolidateViaDeclarations: true,
+		            consolidateViaSelectors:    true,
+		            consolidateMediaQueries:    true
+		        },
+		        files: {
+		            'build/css/master.css': 'build/css/master.css'
+		        }
+		    }
+		},
+
+		cssmin: {
+		    build: {
+		        src: 'build/css/master.css',
+		        dest: 'build/css/master.css'
+		    }
+		},
+
+		sass: {
+		    build: {
+		        files: {
+		            'build/css/master.css': 'assets/sass/master.scss'
+		        }
+		    }
+		},
+
+		watch: {
+		    html: {
+		        files: ['index.html'],
+		        tasks: ['htmlhint']
+		    },
+		    css: {
+		        files: ['assets/sass/master.scss'],
+		        tasks: ['buildcss']
+		    }
+		}
+    });
+
+    grunt.registerTask('default', []);
+    grunt.registerTask('buildcss',  ['sass', 'cssc', 'cssmin']);
+};
