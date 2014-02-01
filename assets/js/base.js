33a0de952d685ee2ffb7f442473e22626d3ab1eb
Ivan Sugerman <ivan@ivansugerman.com>
2014-02-01 17:39:52 -0500

    adjusted bio fade

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,4 +1,8 @@
 window.addEventListener('load', function(){
+
+	var bio = document.getElementsByClassName('bio');
+	bio[0].setAttribute('class', 'bio animated fade-down-in');
+
 	var img = new Image();
 	img.style.display='none';
 	img.src = 'assets/img/ivan-sugerman.jpg';
