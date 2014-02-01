9013f6871d0420a95926de14eb9f100bbbe921c6
Ivan Sugerman <ivan@ivansugerman.com>
2014-02-01 17:56:15 -0500

    adjusted padding smallser screens

--- assets/js/base.js
+++ assets/js/base.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function(){
 
 	var img = new Image();
 	img.style.display='none';
-	img.src = 'assets/img/ivan-sugerman.jpg';
+	img.src = 'assets/img/is.jpg';
 	img.onload = function() {
 		var photo = document.getElementsByClassName('photo');
 		photo[0].setAttribute('class', 'photo animated fade-in');
